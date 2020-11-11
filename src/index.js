3f08714ebfb296ca3d410dab2eb2d98ec37d5a09
trnguyen1510 <trnguyen1510@gmail.com>
2020-11-10 18:01:19 -0800

    added display Forecast function

--- src/index.js
+++ src/index.js
@@ -75,11 +75,63 @@ function search(city) {
   let apiKey = "d80253f7bab7ebfb0d574948c8b564be";
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(cityUrl).then(displayWeather);
+
+  cityUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(cityUrl).then(displayForecast); 
 }
+
 function cityInput(event) {
   event.preventDefault();
   let city = document.querySelector("#search-input").value;
   search(city);
+}
+function searchLocation(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  let apiKey = "d80253f7bab7ebfb0d574948c8b564be";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCityAndTemp);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function formatHours(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10){
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`
+}
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${forecast.weather[0].icon
+      }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
 }
 
 let form = document.querySelector("#search");
